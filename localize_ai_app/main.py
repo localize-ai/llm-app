86b5fb56c32fda88d12afeb109b37ee4361b0bc0
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-11-02 17:06:12 +0700

    feat: add logging configuration and clean up router inclusion in main.py

--- localize_ai_app/main.py
+++ localize_ai_app/main.py
@@ -1,11 +1,14 @@
-from typing import Annotated
-from fastapi import FastAPI, Header
-from router import  places_router
+import logging
+from fastapi import FastAPI
+from router import places_router
 
 app = FastAPI()
 
-app.include_router(places_router.router, prefix='/v1/places', tags=['places'])
+app.include_router(places_router.router, prefix="/v1/places", tags=["places"])
+
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", port=8080, host="0.0.0.0", reload=True)
