2600b04e4ae63cc0fba71450d4a4182bae95e476
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-11-03 02:38:53 +0700

    feat: update add_places_thumbnail to insert new images at the beginning and remove duplicates while preserving order

--- app/usecase/add_place_thumbail_usecase.py
+++ app/usecase/add_place_thumbail_usecase.py
@@ -5,7 +5,7 @@ def add_places_thumbnail(embeddings: list, places: list):
     # Create a dictionary to map place_id to the corresponding place
     place_dict = {place["_id"]: place for place in places}
 
-    # If the embeddings type is image, input the content on images to the places
+    # Insert new images at the beginning of the images list
     for embedding in embeddings:
         place_id = embedding["place_id"]
         if place_id in place_dict:
@@ -13,11 +13,12 @@ def add_places_thumbnail(embeddings: list, places: list):
             if "images" not in place:
                 place["images"] = []
             if embedding["content"] not in place["images"]:
-                place["images"].append(embedding["content"])
+                place["images"].insert(0, embedding["content"])
 
-    # Remove duplicates from the images list (if necessary)
+    # Remove duplicates from the images list while preserving order
     for place in places:
         if "images" in place:
-            place["images"] = list(set(place["images"]))
+            seen = set()
+            place["images"] = [x for x in place["images"] if not (x in seen or seen.add(x))]
 
     return places
