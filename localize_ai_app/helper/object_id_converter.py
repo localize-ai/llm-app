935866c418ba7cdb5cc0198851bcf59595da3692
Luthfi Arifin <luthfiarifin0222@gmail.com>
2024-11-02 17:03:39 +0700

    feat: add helper function to convert ObjectId to string in documents

---
+++ localize_ai_app/helper/object_id_converter.py
@@ -0,0 +1,9 @@
+from bson import ObjectId
+
+
+def convert_object_id_to_str(doc):
+    if isinstance(doc, dict):
+        for key, value in doc.items():
+            if isinstance(value, ObjectId):
+                doc[key] = str(value)
+    return doc
